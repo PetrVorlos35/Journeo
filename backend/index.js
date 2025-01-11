d68808a8d9624d55ef13afdb5dc0cf793b309164
Petr Vorlíček <petrvorlicek7@gmail.com>
2025-01-11 23:11:50 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://journeo-api.vercel.app/auth/google/callback',
+      callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
       const email = profile.emails[0].value;

71be931f8ca18c841307d24daaaa5409baa6f8d0
Petr Vorlíček <petrvorlicek7@gmail.com>
2025-01-11 23:08:41 +0100

    callback problemy

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+      callbackURL: 'https://journeo-api.vercel.app/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
       const email = profile.emails[0].value;

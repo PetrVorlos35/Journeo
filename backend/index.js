67de5377877d7f22626e1e7202a4ac8a3295372c
Petr Vorlíček <petrvorlicek7@gmail.com>
2025-02-06 20:04:55 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,9 +30,7 @@ app.use('/', userRoutes);
 app.use('/friends', friendsRoutes);
 
 // Server start
-// const PORT = process.env.PORT || 5001;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-module.exports = app;
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

28b2821337f86fb346c86765a56d44129dd622d8
Petr Vorlíček <petrvorlicek7@gmail.com>
2025-03-12 14:13:06 +0100

    oprava?

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createPool({
+const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -12,14 +12,35 @@ const db = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  connectTimeout: 10000, // Increase connection timeout
-});
+  connectTimeout: 10000 // Zvýšení timeoutu pro připojení
+};
 
-db.on('error', (err) => {
-  console.error('Database error:', err);
-  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-    console.error('Database connection lost. Reconnecting...');
-  }
-});
+let db;
+
+function handleDisconnect() {
+  db = mysql.createPool(dbConfig);
+
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.error('Chyba připojení k databázi:', err);
+      setTimeout(handleDisconnect, 5000); // Zkusit znovu po 5 sekundách
+    } else {
+      console.log('Připojeno k databázi.');
+      connection.release();
+    }
+  });
+
+  db.on('error', (err) => {
+    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') {
+      console.error('Spojení ztraceno. Znovu se připojuji...');
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = db;
